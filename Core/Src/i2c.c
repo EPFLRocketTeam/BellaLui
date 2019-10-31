63e24347089943ea8d1d9a125bf0d419a4d03e36
Eazh2000 <eazh.delfosse@gmail.com>
2019-10-31 18:28:09 +0100

    Update i2c

--- Core/Src/i2c.c
+++ Core/Src/i2c.c
@@ -26,6 +26,9 @@
 
 I2C_HandleTypeDef hi2c3;
 DMA_HandleTypeDef hdma_i2c3_rx;
+I2C_HandleTypeDef hfmpi2c1;
+DMA_HandleTypeDef hdma_fmpi2c1_rx;
+
 
 /* I2C3 init function */
 void MX_I2C3_Init(void)
@@ -41,6 +44,25 @@ void MX_I2C3_Init(void)
   hi2c3.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
   hi2c3.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
   if (HAL_I2C_Init(&hi2c3) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+}
+
+void MX_FMPI2C1_Init(void)
+{
+
+  hfmpi2c1.Instance = FMPI2C1;
+  hfmpi2c1.Init.ClockSpeed = 100000;
+  hfmpi2c1.Init.DutyCycle = I2C_DUTYCYCLE_2;
+  hfmpi2c1.Init.OwnAddress1 = 0;
+  hfmpi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+  hfmpi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+  hfmpi2c1.Init.OwnAddress2 = 0;
+  hfmpi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+  hfmpi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+  if (HAL_I2C_Init(&hfmpi2c1) != HAL_OK)
   {
     Error_Handler();
   }
@@ -105,6 +127,63 @@ void HAL_I2C_MspInit(I2C_HandleTypeDef* i2cHandle)
   }
 }
 
+void HAL_FMPI2C_MspInit(I2C_HandleTypeDef* i2cHandle)
+{
+
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  if(i2cHandle->Instance==FMPI2C1)
+  {
+  /* USER CODE BEGIN I2C3_MspInit 0 */
+
+  /* USER CODE END I2C3_MspInit 0 */
+
+    __HAL_RCC_GPIOC_CLK_ENABLE();
+    /**FMPI2C1 GPIO Configuration
+    PC6     ------> FMPI2C1_SCL
+    PC7     ------> FMPI2C1_SDA
+    */
+    GPIO_InitStruct.Pin = GPIO_PIN_6;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_OD;
+    GPIO_InitStruct.Pull = GPIO_PULLUP;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+    GPIO_InitStruct.Alternate = GPIO_AF4_FMPI2C1;
+    HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+    GPIO_InitStruct.Pin = GPIO_PIN_7;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_OD;
+    GPIO_InitStruct.Pull = GPIO_PULLUP;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+    GPIO_InitStruct.Alternate = GPIO_AF4_FMPI2C1;
+    HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+    /* FMPI2C1 clock enable */
+    __HAL_RCC_FMPI2C1_IS_CLK_ENABLED();
+
+    /* FMPI2C1 DMA Init */
+    /* FMPI2C1_RX Init */
+    hdma_fmpi2c1_rx.Instance = DMA1_Stream2;
+    hdma_fmpi2c1_rx.Init.Channel = DMA_CHANNEL_3;
+    hdma_fmpi2c1_rx.Init.Direction = DMA_PERIPH_TO_MEMORY;
+    hdma_fmpi2c1_rx.Init.PeriphInc = DMA_PINC_DISABLE;
+    hdma_fmpi2c1_rx.Init.MemInc = DMA_MINC_ENABLE;
+    hdma_fmpi2c1_rx.Init.PeriphDataAlignment = DMA_PDATAALIGN_BYTE;
+    hdma_fmpi2c1_rx.Init.MemDataAlignment = DMA_MDATAALIGN_BYTE;
+    hdma_fmpi2c1_rx.Init.Mode = DMA_NORMAL;
+    hdma_fmpi2c1_rx.Init.Priority = DMA_PRIORITY_LOW;
+    hdma_fmpi2c1_rx.Init.FIFOMode = DMA_FIFOMODE_DISABLE;
+    if (HAL_DMA_Init(&hdma_fmpi2c1_rx) != HAL_OK)
+    {
+      Error_Handler();
+    }
+
+    __HAL_LINKDMA(i2cHandle,hdmarx,hdma_fmpi2c1_rx);
+
+  /* USER CODE BEGIN FMPI2C1_MspInit 1 */
+
+  /* USER CODE END FMPI2C1_MspInit 1 */
+  }
+}
+
 void HAL_I2C_MspDeInit(I2C_HandleTypeDef* i2cHandle)
 {
 
@@ -131,6 +210,33 @@ void HAL_I2C_MspDeInit(I2C_HandleTypeDef* i2cHandle)
   /* USER CODE END I2C3_MspDeInit 1 */
   }
 } 
+
+void HAL_FMPI2C_MspDeInit(I2C_HandleTypeDef* i2cHandle)
+{
+
+  if(i2cHandle->Instance==FMPI2C1)
+  {
+  /* USER CODE BEGIN FMPI2C1_MspDeInit 0 */
+
+  /* USER CODE END FMPI2C1_MspDeInit 0 */
+    /* Peripheral clock disable */
+	  __HAL_RCC_FMPI2C1_IS_CLK_DISABLE();
+
+    /**FMPI2C1 GPIO Configuration
+    PC6     ------> FMPI2C1_SCL
+    PC7     ------> FMPI2C1_SDA
+    */
+    HAL_GPIO_DeInit(GPIOC, GPIO_PIN_6);
+
+    HAL_GPIO_DeInit(GPIOC, GPIO_PIN_7);
+
+    /* FMPI2C1 DMA DeInit */
+    HAL_DMA_DeInit(i2cHandle->hdmarx);
+  /* USER CODE BEGIN FMPI2C1_MspDeInit 1 */
+
+  /* USER CODE END FMPI2C1_MspDeInit 1 */
+  }
+}
 
 /* USER CODE BEGIN 1 */
 
