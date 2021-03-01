71436652ae4ffd7a9a08c25c62f79e9e64041ad0
karlhajal <karlhajal@gmail.com>
2021-03-01 20:59:09 +0100

    Started refactoring state machine loop intro

--- SW4STM32/BellaLui/Application/HostBoard/Inc/misc/state_machine_helpers.h
+++ SW4STM32/BellaLui/Application/HostBoard/Inc/misc/state_machine_helpers.h
@@ -5,6 +5,9 @@
 
 namespace state_machine_helpers {
     bool handleLiftoffState(const uint32_t currentTime, const uint32_t previousTime);
+    uint8_t newImuDataIsAvailable(const uint32_t currentImuSeqNumber, const uint32_t lastImuSeqNumber);
+    uint8_t newBarometerDataIsAvailable(const uint32_t currentBaroSeqNumber, const uint32_t lastBaroSeqNumber);
+    bool touchdownStateIsReached(const uint32_t currentTime, const uint32_t liftoff_time);
 }
 
 #endif /* MISC_STATE_MACHINE_HELPERS_H_ */
